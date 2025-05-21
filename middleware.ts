9696522785ef87d0b85bee38edd863db97a50326
Kartikeyan Dubey <76141663+kartikeyan-sudo@users.noreply.github.com>
2025-05-21 18:44:22 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -103,7 +103,7 @@ function getCorsHeaders(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    '/admin/:path*'
+    '/admin/:path*',
+    '/api/:path*'
   ]
 };
