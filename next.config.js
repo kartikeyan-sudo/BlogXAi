b8b0545bad54e175b5b2df99493821845a35795e
Kartikeyan Dubey <76141663+kartikeyan-sudo@users.noreply.github.com>
2025-05-21 18:51:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -26,6 +26,7 @@ const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 
   // Disable source maps in production for better performance
